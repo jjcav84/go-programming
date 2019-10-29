004264579eb32036eb7dc9d124116854fe57feed
Jacob Cavazos <jjcav84@gmail.com>
2019-10-29 12:43:53 -0500

    short declaration operator code

---
+++ section4/short-declaration-operator/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 42 // := declares the variable AND assigns the value in a singular operator
+	fmt.Println(x)
+	x = 99
+	fmt.Println(x)
+	y := 100 + 42
+	fmt.Println(y)
+	z := "Bond, James"
+	fmt.Println(z)
+}
